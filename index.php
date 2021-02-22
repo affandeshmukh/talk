7f1aa603e52f6181650210530b7b54a67404c174
affandeshmukh <69684368+affandeshmukh@users.noreply.github.com>
2021-02-22 11:42:18 +0530

    Update index.php

--- index.php
+++ index.php
@@ -8,8 +8,7 @@
     <title>Group Chat</title>
     
     <link rel="stylesheet" href="style.css" type="text/css" />
-    
-<script data-ad-client="ca-pub-1914156115794449" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+    <script data-ad-client="ca-pub-4826424761287686" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>script>
     <script type="text/javascript" src="chat.js"></script>
     <script type="text/javascript">
     
